6887b793f539c2ca91746776751c82e636db14c1
Justin Azoff <justin@bouncybouncy.net>
2012-10-02 21:32:19 -0400

    implement uploading benchmarking too

--- http_flood.go
+++ http_flood.go
@@ -67,6 +67,30 @@ func Flood(w http.ResponseWriter, req *http.Request) {
     fmt.Printf("flood %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
 }
 
+func Upload(w http.ResponseWriter, req *http.Request) {
+    buf := make([]byte, blocksize)
+    fmt.Printf("upload starting addr=%s\n", req.RemoteAddr)
+    start := time.Now()
+    status := "finished"
+    var written uint64 = 0
+
+    for {
+        nr, er :=  req.Body.Read(buf)
+        written += uint64(nr)
+        if er != nil {
+            break
+        }
+        runtime.Gosched()
+    }
+    duration := time.Since(start)
+    megabytes := float64(written) / MEGABYTE
+    mbs := megabytes / duration.Seconds()
+    message := fmt.Sprintf("upload %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
+    fmt.Print(message)
+    io.WriteString(w, message)
+}
+
+
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     var port int
@@ -82,6 +106,7 @@ func main() {
     }
     myHandler.HandleFunc("/", Hello)
     myHandler.HandleFunc("/flood", Flood)
+    myHandler.HandleFunc("/upload", Upload)
     fmt.Printf("Listening on port %d\n", port)
     s.ListenAndServe()
 }
