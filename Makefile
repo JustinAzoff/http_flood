524cc2f20ea3bb6499e915401d0dc04b22b6d945
Justin Azoff <justin@bouncybouncy.net>
2012-10-22 20:38:06 -0400

    added client

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: http_flood
+all: http_flood client
 
 http_flood: http_flood.go
 	go build http_flood.go
+
+client: client.go
+	go build client.go
 
 run: http_flood
 	./http_flood
---
+++ client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "time"
+)
+
+const blocksize = 8 * 1024
+const MEGABYTE = 1024 * 1024
+
+func download(host string, megs int) {
+    status := "finished"
+    buffer := make([]byte, blocksize)
+    url := fmt.Sprintf("http://%s/flood?m=%d", host, megs)
+
+    read := 0
+    start := time.Now()
+    resp, err := http.Get(url)
+    defer resp.Body.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    for {
+        n, err := io.ReadAtLeast(resp.Body, buffer, blocksize)
+        if err != nil {
+            if err != io.EOF {
+                status = "aborted"
+            }
+            break
+        }
+        read += n
+
+    }
+    duration := time.Since(start)
+    megabytes := float64(read) / MEGABYTE
+    mbs := megabytes / duration.Seconds()
+
+    fmt.Printf("flood %s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, duration, megabytes, mbs)
+}
+
+func main() {
+    var megabytes int
+    var host string
+    flag.IntVar(&megabytes, "megs", 1024, "megabytes to download")
+    flag.StringVar(&host, "host", "localhost:7070", "Host to connect to")
+    flag.Parse()
+
+    download(host, megabytes)
+}
