0a079b63376a32eac5ccb5f9a63d1ceff0e458d3
Justin <justin@x230.(none)>
2012-10-27 13:34:06 -0400

    refmt

--- client.go
+++ client.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "time"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
 )
 
 const blocksize = 8 * 1024
 const MEGABYTE = 1024 * 1024
 
 func download(host string, megs int) {
-    status := "finished"
-    buffer := make([]byte, blocksize)
-    url := fmt.Sprintf("http://%s/flood?m=%d", host, megs)
+	status := "finished"
+	buffer := make([]byte, blocksize)
+	url := fmt.Sprintf("http://%s/flood?m=%d", host, megs)
 
-    var read uint64 = 0
-    start := time.Now()
-    resp, err := http.Get(url)
-    defer resp.Body.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        n, err := io.ReadAtLeast(resp.Body, buffer, blocksize)
-        if err != nil {
-            if err != io.EOF {
-                status = "aborted"
-            }
-            break
-        }
-        read += uint64(n)
+	var read uint64 = 0
+	start := time.Now()
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		n, err := io.ReadAtLeast(resp.Body, buffer, blocksize)
+		if err != nil {
+			if err != io.EOF {
+				status = "aborted"
+			}
+			break
+		}
+		read += uint64(n)
 
-    }
-    duration := time.Since(start)
-    megabytes := float64(read) / MEGABYTE
-    mbs := megabytes / duration.Seconds()
+	}
+	duration := time.Since(start)
+	megabytes := float64(read) / MEGABYTE
+	mbs := megabytes / duration.Seconds()
 
-    fmt.Printf("flood %s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, duration, megabytes, mbs)
+	fmt.Printf("flood %s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, duration, megabytes, mbs)
 }
 
 func main() {
-    var megabytes int
-    var host string
-    flag.IntVar(&megabytes, "megs", 1024, "megabytes to download")
-    flag.StringVar(&host, "host", "localhost:7070", "Host to connect to")
-    flag.Parse()
+	var megabytes int
+	var host string
+	flag.IntVar(&megabytes, "megs", 1024, "megabytes to download")
+	flag.StringVar(&host, "host", "localhost:7070", "Host to connect to")
+	flag.Parse()
 
-    download(host, megabytes)
+	download(host, megabytes)
 }
--- http_flood.go
+++ http_flood.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "crypto/rand"
-    "flag"
-    "fmt"
-    "io"
-    "net/http"
-    "runtime"
-    "strconv"
-    "time"
-    "html/template"
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 const blocksize = 8 * 1024
@@ -34,91 +34,90 @@ var indexTemplate = template.Must(template.New("").Parse(`
 </html>`))
 
 func Hello(w http.ResponseWriter, req *http.Request) {
-    indexTemplate.Execute(w, len(connections))
+	indexTemplate.Execute(w, len(connections))
 }
 
 func Flood(w http.ResponseWriter, req *http.Request) {
-    connections[req] = true
-    defer delete(connections, req)
+	connections[req] = true
+	defer delete(connections, req)
 
-    random_bytes := make([]byte, blocksize)
-    _, err := rand.Read(random_bytes)
-    if err != nil {
-        return
-    }
+	random_bytes := make([]byte, blocksize)
+	_, err := rand.Read(random_bytes)
+	if err != nil {
+		return
+	}
 
-    ms := req.FormValue("m")
-    if ms == "" {
-        ms = "1"
-    }
-    m, err := strconv.ParseUint(ms, 10, 0)
-    if err != nil {
-        m = 1
-    }
+	ms := req.FormValue("m")
+	if ms == "" {
+		ms = "1"
+	}
+	m, err := strconv.ParseUint(ms, 10, 0)
+	if err != nil {
+		m = 1
+	}
 
-    fmt.Printf("flood starting addr=%s megabytes=%d\n", req.RemoteAddr, m)
-    start := time.Now()
-    status := "finished"
-    var written uint64 = 0
+	fmt.Printf("flood starting addr=%s megabytes=%d\n", req.RemoteAddr, m)
+	start := time.Now()
+	status := "finished"
+	var written uint64 = 0
 
-    w.Header().Set("Content-length", strconv.FormatUint(m*MEGABYTE, 10))
-    for ; written < m*MEGABYTE; written += blocksize {
-        _, err := w.Write(random_bytes)
-        if err != nil {
-            status = "aborted"
-            break
-        }
-        runtime.Gosched()
-    }
-    duration := time.Since(start)
-    megabytes := float64(written) / MEGABYTE
-    mbs := megabytes / duration.Seconds()
-    fmt.Printf("flood %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
+	w.Header().Set("Content-length", strconv.FormatUint(m*MEGABYTE, 10))
+	for ; written < m*MEGABYTE; written += blocksize {
+		_, err := w.Write(random_bytes)
+		if err != nil {
+			status = "aborted"
+			break
+		}
+		runtime.Gosched()
+	}
+	duration := time.Since(start)
+	megabytes := float64(written) / MEGABYTE
+	mbs := megabytes / duration.Seconds()
+	fmt.Printf("flood %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
 }
 
 func Upload(w http.ResponseWriter, req *http.Request) {
-    connections[req] = true
-    defer delete(connections, req)
+	connections[req] = true
+	defer delete(connections, req)
 
-    buf := make([]byte, blocksize)
-    fmt.Printf("upload starting addr=%s\n", req.RemoteAddr)
-    start := time.Now()
-    status := "finished"
-    var written uint64 = 0
+	buf := make([]byte, blocksize)
+	fmt.Printf("upload starting addr=%s\n", req.RemoteAddr)
+	start := time.Now()
+	status := "finished"
+	var written uint64 = 0
 
-    for {
-        nr, er :=  req.Body.Read(buf)
-        written += uint64(nr)
-        if er != nil {
-            break
-        }
-        runtime.Gosched()
-    }
-    duration := time.Since(start)
-    megabytes := float64(written) / MEGABYTE
-    mbs := megabytes / duration.Seconds()
-    message := fmt.Sprintf("upload %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
-    fmt.Print(message)
-    io.WriteString(w, message)
+	for {
+		nr, er := req.Body.Read(buf)
+		written += uint64(nr)
+		if er != nil {
+			break
+		}
+		runtime.Gosched()
+	}
+	duration := time.Since(start)
+	megabytes := float64(written) / MEGABYTE
+	mbs := megabytes / duration.Seconds()
+	message := fmt.Sprintf("upload %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
+	fmt.Print(message)
+	io.WriteString(w, message)
 }
 
-
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    var port int
-    flag.IntVar(&port, "port", 7070, "Port to bind to")
-    flag.Parse()
-    myHandler := http.NewServeMux()
-    s := &http.Server{
-        Addr:           fmt.Sprintf(":%d", port),
-        Handler:        myHandler,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   60 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
-    myHandler.HandleFunc("/", Hello)
-    myHandler.HandleFunc("/flood", Flood)
-    myHandler.HandleFunc("/upload", Upload)
-    fmt.Printf("Listening on port %d\n", port)
-    s.ListenAndServe()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var port int
+	flag.IntVar(&port, "port", 7070, "Port to bind to")
+	flag.Parse()
+	myHandler := http.NewServeMux()
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		Handler:        myHandler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   60 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	myHandler.HandleFunc("/", Hello)
+	myHandler.HandleFunc("/flood", Flood)
+	myHandler.HandleFunc("/upload", Upload)
+	fmt.Printf("Listening on port %d\n", port)
+	s.ListenAndServe()
 }
