2eaa0311f04177bef5d6b3338a165bfc879d65b0
Justin <justin@x230.(none)>
2012-10-28 14:26:52 -0400

    implement upload support

--- client.go
+++ client.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -39,7 +41,52 @@ func download(host string, megs int) {
 	megabytes := float64(read) / MEGABYTE
 	mbs := megabytes / duration.Seconds()
 
-	fmt.Printf("flood %s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, duration, megabytes, mbs)
+	fmt.Printf("download %s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, duration, megabytes, mbs)
+}
+
+type LoopReader struct {
+	pos  int
+	buf  []byte
+	size uint64
+}
+
+func NewLooper(megs int) *LoopReader {
+	random_bytes := make([]byte, blocksize)
+	_, err := rand.Read(random_bytes)
+	if err != nil {
+		return nil
+	}
+
+	return &LoopReader{
+		buf:  random_bytes,
+		size: uint64(megs) * MEGABYTE,
+	}
+}
+
+func (r *LoopReader) Read(p []byte) (n int, err error) {
+	n = len(p)
+	toread := n
+	if n > len(r.buf) {
+		toread = len(r.buf)
+	}
+	if uint64(toread) > r.size {
+		toread = int(r.size)
+	}
+	r.size -= uint64(toread)
+
+	copy(p[0:toread], r.buf[0:toread])
+	if r.size == 0 {
+		return toread, io.EOF
+	}
+	return toread, nil
+}
+
+func upload(host string, megs int) {
+	url := fmt.Sprintf("http://%s/upload?m=%d", host, megs)
+
+	resp, _ := http.Post(url, "application/octet-stream", NewLooper(megs))
+
+	io.Copy(os.Stdout, resp.Body)
 }
 
 func main() {
@@ -50,4 +97,6 @@ func main() {
 	flag.Parse()
 
 	download(host, megabytes)
+	upload(host, megabytes)
+
 }
