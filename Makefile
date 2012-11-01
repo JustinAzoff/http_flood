5c2fba4d4ec5819bc4fd820ff6dda4f745043721
Justin <justin@x230.(none)>
2012-10-31 20:35:01 -0400

    move the server back into the top level directory

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-all: http_flood/http_flood http_flood_client/http_flood_client
+all: http_flood http_flood_client/http_flood_client
 
-http_flood/http_flood: http_flood/http_flood.go common/common.go consts/consts.go
-	cd http_flood && go build
+http_flood: http_flood.go common/common.go consts/consts.go
+	go build
 
 http_flood_client/http_flood_client: http_flood_client/client.go common/common.go consts/consts.go
 	cd http_flood_client/ && go build
 
-run: http_flood/http_flood
-	./http_flood/http_flood
+run: http_flood
+	./http_flood
---
+++ http_flood.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"./common"
+	"./consts"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+var connections = make(map[*http.Request]bool)
+
+var indexTemplate = template.Must(template.New("").Parse(`
+<html>
+<head><title>HTTP Flood Server </title></head>
+<body>
+<h1>HTTP Flood Server</h1>
+<ul>
+ <li><a href="/flood?m=1">1 megabyte file</a></li>
+ <li><a href="/flood?m=10">10 megabyte file</a></li>
+ <li><a href="/flood?m=100">100 megabyte file</a></li>
+ <li><a href="/flood?m=1024">1 gigabyte file</a></li>
+ <li><a href="/flood?m=10240">10 gigabyte file</a></li>
+</ul>
+</body>
+<p> Current connections: {{.}} </p>
+</html>`))
+
+func Hello(w http.ResponseWriter, req *http.Request) {
+	indexTemplate.Execute(w, len(connections))
+}
+
+func Flood(w http.ResponseWriter, req *http.Request) {
+	connections[req] = true
+	defer delete(connections, req)
+
+	ms := req.FormValue("m")
+	if ms == "" {
+		ms = "1"
+	}
+	m, err := strconv.ParseUint(ms, 10, 0)
+	if err != nil {
+		m = 1
+	}
+
+	fmt.Printf("flood starting addr=%s megabytes=%d\n", req.RemoteAddr, m)
+	start := time.Now()
+	status := "finished"
+	w.Header().Set("Content-length", strconv.FormatUint(m*consts.Megabyte, 10))
+	written, err := io.Copy(w, common.LimitedRandomGen(m*consts.Megabyte))
+
+	if err != nil {
+		status = "aborted"
+	}
+	duration := time.Since(start)
+	megabytes := float64(written) / consts.Megabyte
+	mbs := megabytes / duration.Seconds()
+	fmt.Printf("flood %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
+}
+
+func Upload(w http.ResponseWriter, req *http.Request) {
+	connections[req] = true
+	defer delete(connections, req)
+
+	fmt.Printf("upload starting addr=%s\n", req.RemoteAddr)
+	start := time.Now()
+	status := "finished"
+
+	written, err := io.Copy(ioutil.Discard, req.Body)
+	if err != nil {
+		status = "aborted"
+	}
+
+	duration := time.Since(start)
+	megabytes := float64(written) / consts.Megabyte
+	mbs := megabytes / duration.Seconds()
+	message := fmt.Sprintf("upload %s addr=%s duration=%s megabytes=%.1f speed=%.1fMB/s\n", status, req.RemoteAddr, duration, megabytes, mbs)
+	fmt.Print(message)
+	io.WriteString(w, message)
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	addr := flag.String("addr", ":7070", "Address to bind to")
+	flag.Parse()
+	myHandler := http.NewServeMux()
+	s := &http.Server{
+		Addr:           *addr,
+		Handler:        myHandler,
+		ReadTimeout:    60 * time.Second,
+		WriteTimeout:   60 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	myHandler.HandleFunc("/", Hello)
+	myHandler.HandleFunc("/flood", Flood)
+	myHandler.HandleFunc("/upload", Upload)
+	fmt.Printf("Listening on %s\n", *addr)
+	log.Fatal(s.ListenAndServe())
+}
