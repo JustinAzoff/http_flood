71290375d3ffadf817c19500a83b62edbfb02872
Justin Azoff <justin@bouncybouncy.net>
2013-02-25 21:36:12 -0500

    add support for timed flooding

--- common/common.go
+++ common/common.go
@@ -4,6 +4,7 @@ import (
 	"../consts"
 	"crypto/rand"
 	"io"
+	"time"
 )
 
 type RandomGen struct {
@@ -34,5 +35,28 @@ func (r *RandomGen) Read(p []byte) (n int, err error) {
 }
 
 func LimitedRandomGen(n uint64) io.Reader {
-    return io.LimitReader(NewRandomGen(), int64(n))
+	return io.LimitReader(NewRandomGen(), int64(n))
+}
+
+func NewTimedReader(r io.Reader, n uint64) io.Reader {
+	start := time.Now()
+	end := start.Add(time.Duration(n) * time.Second)
+	return &TimedReader{r, end}
+}
+
+type TimedReader struct {
+	R   io.Reader // underlying reader
+	End time.Time //When to stop reading
+}
+
+func (l *TimedReader) Read(p []byte) (n int, err error) {
+	if time.Now().After(l.End) {
+		return 0, io.EOF
+	}
+	n, err = l.R.Read(p)
+	return
+}
+
+func TimedRandomGen(s uint64) io.Reader {
+	return NewTimedReader(NewRandomGen(), s)
 }
